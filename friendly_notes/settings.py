14d7ee59e85d4778a97af1cee6bdfd5baaedcf2d
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-08-14 11:58:01 +0200

    Load the .env file in settings.py

--- friendly_notes/settings.py
+++ friendly_notes/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,10 +26,13 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ur!s@j#e1l%+)80)nm=*93-75bu6nu6(*$8oqz)iz8$gm-&tgv'
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    'ut90q3thjuy08943qwah4rq-3t8jhfwqoiefhweoiufyhweug3-wheiorhuyewuoioidyfpo'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 0)
 
 ALLOWED_HOSTS = []
 
