0abb48a3e79ab493cba1303e3719c8bbee42a68a
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-08-14 12:06:49 +0200

    Change db from sqlite3 to postgresql

--- friendly_notes/settings.py
+++ friendly_notes/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'friendly_notes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
