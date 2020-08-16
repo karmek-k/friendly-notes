9bf7cf99cb9fdd8c62e1041e3ed6de1620aae403
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-08-16 14:57:08 +0200

    Remove commented out code

--- note/views.py
+++ note/views.py
@@ -1,5 +1,4 @@
 from django.views import generic
-from django.urls import reverse_lazy
 
 from note.models import Note
 from note.forms import NoteForm
@@ -12,7 +11,6 @@ class IndexView(generic.TemplateView):
 class AddNoteView(generic.CreateView):
     form_class = NoteForm
     template_name = 'note/add_note.html'
-    # success_url = reverse_lazy('note:get')
 
 
 class GetNoteView(generic.DetailView):
